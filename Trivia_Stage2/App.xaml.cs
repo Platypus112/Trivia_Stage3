ae317c75368a3974659ff064ab6cadc1defaa839
User <User@LAB3-3>
2024-02-20 14:10:11 +0200

    Add project files.

---
+++ Trivia_Stage2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Trivia_Stage2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Trivia_Stage2/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Trivia_Stage2
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
