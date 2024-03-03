388c53fa2928191c9e0791ae701dc0ddb7428552
Stom <Stom@COMPUTER>
2024-03-03 21:37:33 +0200

    best scores update

--- Trivia_Stage2/ViewModels/BestScoresPageVIewModel.cs
+++ Trivia_Stage2/ViewModels/BestScoresPageVIewModel.cs
@@ -1,18 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Trivia_Stage2.Models;
 using Trivia_Stage2.Services;
+
 
 namespace Trivia_Stage2.ViewModels
 {
     public class BestScoresPageViewModel : ViewModel
     {
         private Service service;
+        public bool IsReloading;
+        public ICommand Reload {  get; set; }
+        public ICommand Search {  get; set; }
+        public string SearchBar;
+        public bool IsOrdered;
+
+        public ObservableCollection<Player> players { get; private set; }
+
         public BestScoresPageViewModel(Service service_)
         {
+            IsOrdered = false;
             service = service_;
+            players = new ObservableCollection<Player>(service.Players);
+            IsReloading = false;
+            Reload=new Command(async ()=> await TaskReload());
+            Search = new Command(async () => await TaskSearch();
+        }
+        public async Task TaskReload()
+        {
+            if(IsReloading) return;
+            IsReloading=true;
+            players.Clear();
+            foreach(Player plr in service.players)
+                players.add(plr);
+            if (IsOrdered)
+            {
+                players = new ObservableCollection<Player>(players.OrderByDescending(player p => p.Points));
+                IsOrdered = false;
+            }
+            else
+            {
+                players = new ObservableCollection<Player>(players.OrderByAscending(player p => p.Points));
+                IsOrdered = true;
+            }
+            IsReloading = false;
+
+        }
+        public async Task TaskSearch()
+        {
+            if (int.TryParse(SearchBar, out int search))
+            {
+                players = new ObservableCollection<Player>(players.Where(player => player.Points == search));
+            }
         }
     }
 }
