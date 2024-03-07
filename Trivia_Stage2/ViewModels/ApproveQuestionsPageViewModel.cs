02c57408a1b411ca41c5a9db2df533d8a80867fe
User <User@LAB1-05>
2024-03-07 12:17:06 +0200

    les go

--- Trivia_Stage2/ViewModels/ApproveQuestionsPageViewModel.cs
+++ Trivia_Stage2/ViewModels/ApproveQuestionsPageViewModel.cs
@@ -1,5 +1,4 @@
-﻿using Android.Widget;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -23,31 +22,35 @@ namespace Trivia_Stage2.ViewModels
         public Question SelectedPendingQuestion { get => selectedPendingQuestion; set { selectedPendingQuestion = value; OnPropertyChanged(); } }
         private bool isRefreshing;
         public bool IsRefreshing { get => isRefreshing; set { isRefreshing = value; OnPropertyChanged(); } }
-         private string filterEntry;
+        private string filterEntry;
         public string FilterEntry { get => filterEntry; set{ filterEntry = value; OnPropertyChanged(); } }
-       
-
-
-
+        public List<Subject> subjects { get; private set; }
+        private Subject selectedSubject;
+        public Subject SelectedSubject { get => selectedSubject; set { selectedSubject = value; OnPropertyChanged(); } }
         public ICommand DeclineQuestionCommand { get; private set; }
         public ICommand ApproveQuestionCommand { get; private set; }
         public ICommand FilterBySubjectCommand { get; private set; }
         public ICommand LoadPendingQuestionsCommand { get; private set; }
         public ApproveQuestionsPageViewModel(Service service_)
         {
+            //Question q in service.Questions.Where(x => x.SubjectId == SelectedSubject.SubjectId)
             service = service_;
+            subjects = new();
             pendingQuestionKeeper = service.GetPendingQuestions();
             PendingQuestions = new ObservableCollection<Question>(service.GetPendingQuestions());
             DeclineQuestionCommand = new Command(async (Object obj) => { await DeclineQuestion(obj); });
             ApproveQuestionCommand = new Command(async (Object obj) => await ApproveQuestion(obj));
-            FilterBySubjectCommand = new Command<string>((x) => FilterSubject(x));
+            FilterBySubjectCommand = new Command(() => FilterSubject());
             LoadPendingQuestionsCommand = new Command(async () => { await LoadPendingQuestions();});
+            foreach (Subject s in service.Subjects)
+            {
+                subjects.Add(s);
+            }
         }
 
         private async Task ApproveQuestion(Object obj)
         {
-            bool approve = await AppShell.Current.DisplayAlert("Question", "Would you like to approve the question?", "Yes", "Cancel");
-            if (approve == true)
+            if (await AppShell.Current.DisplayAlert("Question", "Would you like to approve the question?", "Yes", "Cancel"))
             {
                 PendingQuestions.Remove(((Question)obj));
                 service.ApproveQuestion(((Question)obj));
@@ -56,20 +59,17 @@ namespace Trivia_Stage2.ViewModels
         
         private async Task DeclineQuestion(Object obj)
         {
-            bool approve = await AppShell.Current.DisplayAlert("Question", "Would you like to decline the question?", "Yes", "Cancel");
-            if (approve == true)
+            if (await AppShell.Current.DisplayAlert("Question", "Would you like to decline the question?", "Yes", "Cancel"))
             {
                 PendingQuestions.Remove(((Question)obj));
                 service.DeclineQuestion(((Question)obj));
             }
         }
 
-        private void FilterSubject(string filter)
+        private void FilterSubject()
         {
-            
-            List<Question> filteredQ = service.GetPendingQuestionsBySubjectName(filter);
             PendingQuestions.Clear();
-            foreach (Question q in filteredQ)
+            foreach (Question q in service.Questions.Where(x => x.SubjectId == SelectedSubject.SubjectId))
             {
                 PendingQuestions.Add(q);
             }
